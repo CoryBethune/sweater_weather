f3c2871438ddc5f1973ee9243d219e990f740ce1
Cory Bethune <corybbethune@gmail.com>
2022-09-26 01:46:29 -0700

    Location poro passes test.

--- app/poro/location.rb
+++ app/poro/location.rb
@@ -3,6 +3,6 @@ class Location
 
   def initialize(data)
     @lat = data[:results][0][:locations][0][:latLng][:lat]
-    @long = data[:results][0][:locations][0][:latLng][:lng]
+    @lon = data[:results][0][:locations][0][:latLng][:lng]
   end
 end
