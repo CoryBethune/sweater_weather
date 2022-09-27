c4b5ba26754150c1172df6f619a7dd153e8d1995
Cory Bethune <corybbethune@gmail.com>
2022-09-27 10:22:14 -0700

    Brewery Serializer done

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -4,6 +4,6 @@ class Api::V1::BreweriesController < ApplicationController
     location = MapQuestFacade.get_location(params[:location])
     forecast = OpenWeatherFacade.get_weather(location.lat, location.lon)
     breweries = BreweriesFacade.get_breweries(params[:location], params[:quantity])
-    render json: BreweriesSerializer.new_breweries(forecast, breweries)
+    render json: BreweriesSerializer.new_breweries(forecast, breweries, params[:location])
   end
 end
--- app/serializers/breweries_serializer.rb
+++ app/serializers/breweries_serializer.rb
@@ -9,7 +9,14 @@ class BreweriesSerializer
           forecast: {
             summary: forecast.current[:weather].first[:description],
             temperature: forecast.current[:temp].to_s + " F"
-          }
+          },
+          breweries: breweries.all.map do |brewery|
+            {
+              id: brewery[:id],
+              name: brewery[:name],
+              brewery_type: brewery[:brewery_type]
+            }
+          end
         }
       }
     }
