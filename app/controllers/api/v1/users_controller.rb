c4f95bdb7820b43610d1f031690d0e347ae38b04
Cory Bethune <corybbethune@gmail.com>
2022-09-28 13:52:41 -0700

    User controller now has error handling.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,14 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     if params[:password] == params[:password_confirmation]
-      user = User.create!(email: params[:email], password: params[:password])
-      render json: UserSerializer.new_user(user), status: 201
+      user = User.new(email: params[:email], password: params[:password])
+      if user.save
+        render json: UserSerializer.new_user(user), status: 201
+      else
+        render json: { errors: user.errors.full_messages }, status: 401
+      end
+    else
+      render json: { errors: "Password and password confirmation fields must match." }, status: 401
     end
   end
 
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe "User Creation" do
           "email": "test@example.com",
           "password": "test123",
           "password_confirmation": "test123"
-        })
-        headers = {"CONTENT_TYPE" => "application/json"}
-        post "/api/v1/users", headers: headers, params: JSON.generate(params)
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(params)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    binding.pry
 
 
   end
